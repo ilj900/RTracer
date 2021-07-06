56c0e9a4809b6f57049e0a6458331d6083e72d0b
Roman <ilj900@gmail.com>
2021-07-06 22:53:08 +0300

    GLFW.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include "entity.h"
+#include "GLFW/glfw3.h"
 
 #include <iostream>
+#include <string>
+
+const uint32_t WINDOW_WIDTH = 1920;
+const uint32_t WINDOW_HEIGHT = 1080;
+const std::string WINDOW_NAME = "RTracer";
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+    GLFWwindow* Window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME.c_str(), nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(Window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(Window);
+    glfwTerminate();
     return 0;
 }
