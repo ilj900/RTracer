65eb83a0038fecc87f7ac5fc05d44a05852ee3a0
Roman <ilj900@gmail.com>
2022-04-05 17:51:58 +0300

    Added dummy Readme.md

--- ECS/entities/entity.h
+++ ECS/entities/entity.h
@@ -4,17 +4,29 @@
 
 namespace ECS
 {
-    /// Entity is just a custom type for uint16
+    /**
+    * Entity is just a custom type for uint16. This also defines the maximum number of entities
+    */
     using FEntity = std::uint16_t;
-    /// MAX_ENTITIES = std::numeric_limits<uint16_t>::max();
-    constexpr std::uint32_t MAX_ENTITIES = std::numeric_limits<uint16_t>::max();
 
-    /// Component type is just a custom type for uint8
+    /**
+     * It also can be limited by any hardcoded value.
+     */
+    constexpr std::uint32_t MAX_ENTITIES = std::numeric_limits<FEntity>::max();
+
+     /**
+     * Component type is just a custom type for uint8
+     */
     using FComponentType = std::uint8_t;
-    /// MAX_COMPONENTS = std::numeric_limits<uint8_t>::max();
-    const std::uint8_t MAX_COMPONENTS = std::numeric_limits<uint8_t>::max();
 
-    /// Signature is a bitset, that stores information about the component's that attached to entity
+     /**
+     * As MAX_ENTITIES, it also can be haedcoded by any other value
+     */
+    const std::uint8_t MAX_COMPONENTS = std::numeric_limits<FComponentType>::max();
+
+    /**
+     * Signature is a bitset, that stores information about the component's that attached to entity
+     */
     using FSignature = std::bitset<MAX_COMPONENTS>;
 
 }
---
+++ Readme.md
@@ -0,0 +1,12 @@
+![image info](./resources/Screenshot.png)
+
+## What is it?
+This is my pet-project renderer. Just to learn and test something new.
+
+## Requisits
+Vulkan SDK 1.2+
+
+CMake 3.2+
+
+## Build
+Just open CMakeLists.txt in something like CLion.
